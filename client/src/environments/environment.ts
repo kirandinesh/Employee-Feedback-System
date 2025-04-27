2ab0ac999e213860b3ac4ff1bd6be3f796effb0c
Kirandinesh <kirandinesh646@gmail.com>
2025-04-27 21:47:10 +0530

    update in env

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  authApiUrl: 'https://employee-feedback-system-wskl.onrender.com/auth',
+  reviewApiUrl: 'https://employee-feedback-system-wskl.onrender.com/review',
+};
